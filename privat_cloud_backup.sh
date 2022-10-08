f1ee1be8f1f680e9ed9bd28aff1288f4ddfa72e4
Volokzhanin Vadim <volokzhanin@yandex.ru>
2022-10-08 15:19:46 +0300

    rename project

--- privat_cloud_backup.sh
+++ privat_cloud_backup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Создаем резервную копию
-cd /home/ubuntu/docker/nextcloud
-tar cvpzf /mnt/backup/backup/vvy_nextcloud/"$(date '+%Y-%m-%d').tar.gz" ./
+cd /home/ubuntu/docker/privat_cloud
+tar cvpzf /mnt/backup/backup/vvy_privat_cloud/"$(date '+%Y-%m-%d').tar.gz" ./
 # Удаляем архивы резервной копии старше n дней
-find /mnt/backup/backup/vvy_nextcloud/ -mtime +3 -type f -delete
+find /mnt/backup/backup/vvy_privat_cloud/ -mtime +3 -type f -delete
 
 # restore 
-# sudo rsync -arvh --progress /mnt/backup/backup/vvy_nextcloud/2021-06-20/ ./
+# sudo rsync -arvh --progress /mnt/backup/backup/vvy_privat_cloud/2021-06-20/ ./
 # Получаем container id 
 # docker ps
 # Заходим в контейнер 
